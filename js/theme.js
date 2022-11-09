00b4211136e6a25f998d0f67806f9a4e90865f7f
Umang Bhalodiya <umangbhalodiya660@gmail.com>
2022-11-09 20:32:18 +0530

    default theme added

--- js/theme.js
+++ js/theme.js
@@ -1,7 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const input = document.querySelector('#themeInput')
+document.addEventListener("DOMContentLoaded", () => {
+  const input = document.querySelector("#themeInput");
   const switchTheme = ({ target: { checked } }) =>
-    document.documentElement.setAttribute('data-theme', checked ? 'dark' : 'light')
+    document.documentElement.setAttribute(
+      "data-theme",
+      checked ? "dark" : "light"
+    );
 
-  input.addEventListener('change', switchTheme)
-})
+  input.addEventListener("change", switchTheme);
+});
